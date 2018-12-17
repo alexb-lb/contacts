88225928b605ce3024a08b45a2c27eea6f243a68
alex <oknerbob@gmail.com>
2018-12-17 14:20:22 +0200

    app moved to heroku. dev vars changes

--- server/modules/db.js
+++ server/modules/db.js
@@ -3,9 +3,7 @@ const Sequelize = require('sequelize');
 // plug in the promise library:
 Sequelize.Promise = global.Promise;
 
-// const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
 const db = new Sequelize(process.env.DB_URI, {
-  // host: process.env.DB_URI,
   dialect: process.env.DB_DIALECT,
   logging: false,
   returning: true,
